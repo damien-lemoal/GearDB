4d766aea11e73bb9071cd027be42ce1f71c1cad5
Damien Le Moal <damien.lemoal@wdc.com>
2019-03-05 17:59:09 +0900

    HM: code cleanup
    Identation and style fixes. Also fixed some compilation warnings.
    
    Signed-off-by: Damien Le Moal <damien.lemoal@wdc.com>

--- hm/hm_manager.h
+++ hm/hm_manager.h
@@ -9,99 +9,102 @@
 #include <stdlib.h>
 #include <cstring>
 #include <map>
-#include <vector>  
+#include <vector>
 
 #include "../db/dbformat.h"
 #include "../hm/my_log.h"
 #include "../hm/BitMap.h"
 #include "../hm/hm_status.h"
 
-
 extern "C" {
 #include <libzbc/zbc.h>
 }
 
 namespace leveldb{
+	class HMManager {
+	public:
+		HMManager(const Comparator *icmp);
+		~HMManager();
 
-    class HMManager {
-    public:
-        HMManager(const Comparator *icmp);
-        ~HMManager();
-        
-        ssize_t hm_write(int level,uint64_t filenum,const void *buf,uint64_t count);   //write a SSTable file to a level
-        ssize_t hm_read(uint64_t filenum,void *buf,uint64_t count, uint64_t offset);   //read a SSTable file
-        ssize_t hm_delete(uint64_t filenum);                                           //delete a SSTable file
-        ssize_t move_file(uint64_t filenum,int to_level);                              //move a SSTable file
-        struct Ldbfile* get_one_table(uint64_t filenum);                               //get a SSTable file pointer
+		// Write a SSTable file to a level
+		ssize_t hm_write(int level, uint64_t filenum,
+				 const void *buf, uint64_t count);
 
+		// Read a SSTable file
+		ssize_t hm_read(uint64_t filenum, void *buf,
+				uint64_t count, uint64_t offset);
 
-        void get_table(std::map<uint64_t, struct Ldbfile*> **table_map){ *table_map=&table_map_; };  //get table_map
-        
-        //////dump relation
-        void get_zone_table(uint64_t filenum,std::vector<struct Ldbfile*> **zone_table);
-        bool trivial_zone_size_move(uint64_t filenum);
-        void move_zone(uint64_t filenum);
-        //////
+		// Delete a SSTable file
+		ssize_t hm_delete(uint64_t filenum);
 
-        //////compaction relation
-        void update_com_window(int level);
-        void get_com_window_table(int level,std::vector<struct Ldbfile*> *window_table);
-        ssize_t adjust_com_window_num(int level);
-        void set_com_window(int level,int num);
-        void set_com_window_seq(int level,int num);
-        //////
+		// Move a SSTable file
+		ssize_t move_file(uint64_t filenum,int to_level);
 
-        //////statistics
-        uint64_t get_zone_num();
-        void get_one_level(int level,uint64_t *table_num,uint64_t *table_size);
-        void get_per_level_info();
-        void get_valid_info();
-        void get_all_info();
-        void get_valid_data();
-        void get_my_info(int num);
-        void get_valid_all_data(int num);
+		// Get a SSTable file pointer
+		struct Ldbfile* get_one_table(uint64_t filenum);
 
-        //////end
+		// Get table_map
+		void get_table(std::map<uint64_t, struct Ldbfile*> **table_map)
+		{
+			*table_map = &table_map_;
+		};
 
-    private:
-        BitMap *bitmap_;
+		// Dump relation
+		void get_zone_table(uint64_t filenum,
+				    std::vector<struct Ldbfile*> **zone_table);
+		bool trivial_zone_size_move(uint64_t filenum);
+		void move_zone(uint64_t filenum);
 
-        struct zbc_device *dev_;
-        struct zbc_zone  *zone_;
-        unsigned int zonenum_;
-        int first_zonenum_;
+		// Compaction relation
+		void update_com_window(int level);
+		void get_com_window_table(int level,
+					  std::vector<struct Ldbfile*> *window_table);
+		ssize_t adjust_com_window_num(int level);
+		void set_com_window(int level, int num);
+		void set_com_window_seq(int level, int num);
 
-        const InternalKeyComparator icmp_;
+		// Statistics
+		uint64_t get_zone_num();
+		void get_one_level(int level, uint64_t *table_num,
+				   uint64_t *table_size);
+		void get_per_level_info();
+		void get_valid_info();
+		void get_all_info();
+		void get_valid_data();
+		void get_my_info(int num);
+		void get_valid_all_data(int num);
 
-        std::map<uint64_t, struct Ldbfile*> table_map_;  //<file number, metadate pointer>
-        std::vector<struct Zonefile*> zone_info_[config::kNumLevels];  //each level of zone
-        std::vector<struct Zonefile*> com_window_[config::kNumLevels]; //each level of compaction window
+	private:
+		BitMap *bitmap_;
 
-        //////statistics
-        uint64_t delete_zone_num;
-        uint64_t all_table_size;
-        uint64_t kv_store_sector;
-        uint64_t kv_read_sector;
-        uint64_t max_zone_num;
-        uint64_t move_file_size;
-        uint64_t read_time;
-        uint64_t write_time;
-        //////end
+		struct zbc_device *dev_;
+		struct zbc_zone  *zone_;
+		unsigned int zonenum_;
+		unsigned int first_zonenum_;
 
-        int set_first_zonenum();
-        ssize_t hm_alloc(int level,uint64_t size);
-        ssize_t hm_alloc_zone();
-        ssize_t hm_free_zone(uint64_t zone);
+		const InternalKeyComparator icmp_;
 
-        //////
-        bool is_com_window(int level,uint64_t zone);
-        //////
+		std::map<uint64_t, struct Ldbfile*> table_map_; //<file number, metadate pointer>
+		std::vector<struct Zonefile*> zone_info_[config::kNumLevels];  //each level of zone
+		std::vector<struct Zonefile*> com_window_[config::kNumLevels]; //each level of compaction window
 
-    };
+		// Statistics
+		uint64_t delete_zone_num;
+		uint64_t all_table_size;
+		uint64_t kv_store_sector;
+		uint64_t kv_read_sector;
+		uint64_t max_zone_num;
+		uint64_t move_file_size;
+		uint64_t read_time;
+		uint64_t write_time;
 
+		unsigned int set_first_zonenum();
+		ssize_t hm_alloc(int level,uint64_t size);
+		ssize_t hm_alloc_zone();
+		void hm_free_zone(uint64_t zone);
 
-
-
+		bool is_com_window(int level,uint64_t zone);
+	};
 }
 
-#endif 
+#endif
--- hm/hm_status.h
+++ hm/hm_status.h
@@ -16,66 +16,62 @@
 #define MEMALIGN_SIZE (sysconf(_SC_PAGESIZE))     //The size of the alignment when applying for memory using posix_memalign
 
 #define Verify_Table 1        //To confirm whether the SSTable is useful, every time an SSTable is written to the disk, \
-                             //it will read the handle of the file and add it to the leveldb's table cache. This is the leveldb's own mechanism;
-                             // 1 means that there is this mechanism; 0 means no such mechanism.
+	//it will read the handle of the file and add it to the leveldb's table cache. This is the leveldb's own mechanism;
+// 1 means that there is this mechanism; 0 means no such mechanism.
 
 #define Read_Whole_Table 1    //1 means that the entire file is read together when reading the SSTable; 0 means that a block is read during the read operation.
 
 #define Find_Table_Old  1     //1 means that when finding key-value, it is the same as the original, a block is read; 0 means that the search key-value is the same as the Read_Whole_Table parameter.
 
 namespace leveldb {
-    
-    static const char smr_filename[]="";   //e.g. smr_filename[]="/dev/sdb1";
 
-    struct Ldbfile {     //file = SSTable ,file Metadata struct
-        uint64_t table;  //file name = fiel serial number
-        uint64_t zone;   //file's zone number
-        uint64_t offset; //file's offset in the zone
-        uint64_t size;   //file's size
-        int      level;  //file in the level number
+	static const char smr_filename[]="/dev/dm-0";   //e.g. smr_filename[]="/dev/sdb1";
 
-        Ldbfile(uint64_t a,uint64_t b,uint64_t c,uint64_t d,int e):table(a),zone(b),offset(c),size(d),level(e){};
-        ~Ldbfile(){};
-    };
+	struct Ldbfile {     //file = SSTable ,file Metadata struct
+		uint64_t table;  //file name = fiel serial number
+		uint64_t zone;   //file's zone number
+		uint64_t offset; //file's offset in the zone
+		uint64_t size;   //file's size
+		int      level;  //file in the level number
 
-    struct Zonefile {    //zone struct
-        uint64_t zone; //zone num 
-        
-        std::vector<struct Ldbfile*> ldb; //SSTable pointers
-        Zonefile(uint64_t a):zone(a){};
-        ~Zonefile(){};
+        	Ldbfile(uint64_t a,uint64_t b,uint64_t c,uint64_t d,int e):table(a),zone(b),offset(c),size(d),level(e){};
+		~Ldbfile(){};
+	};
 
-        void add_table(struct Ldbfile* file){
-            ldb.push_back(file);
-        }
-        void delete_table(struct Ldbfile* file){
-            std::vector<struct Ldbfile*>::iterator it;
-            for(it=ldb.begin();it!=ldb.end();){
-                if((*it)==file){
-                    ldb.erase(it);
-                    return;
-                }
-                else it++;
-            }
-        }
-        uint64_t get_all_file_size(){
-            uint64_t size=0;
-            std::vector<struct Ldbfile*>::iterator it;
-            for(it=ldb.begin();it!=ldb.end();it++){
-                size += (*it)->size;
-            }
-            return size;
-        }
-    };
+	struct Zonefile {    //zone struct
+		uint64_t zone; //zone num
+		std::vector<struct Ldbfile*> ldb; //SSTable pointers
 
+        	Zonefile(uint64_t a):zone(a){};
+		~Zonefile(){};
 
+		void add_table(struct Ldbfile* file)
+		{
+			ldb.push_back(file);
+		}
 
+		void delete_table(struct Ldbfile* file)
+		{
+			std::vector<struct Ldbfile*>::iterator it;
 
+			for (it = ldb.begin(); it != ldb.end(); it++) {
+				if (*it == file) {
+					ldb.erase(it);
+					return;
+				}
+			}
+		}
+
+		uint64_t get_all_file_size(void)
+		{
+			uint64_t size = 0;
+			std::vector<struct Ldbfile*>::iterator it;
+
+			for (it = ldb.begin(); it != ldb.end(); it++)
+				size += (*it)->size;
+			return size;
+		}
+	};
 }
-
-
-
-
-
 
 #endif
